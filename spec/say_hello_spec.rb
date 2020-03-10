b97c17ed080a109b42c5d4bf82ecf0fc5c894108
galo velastegui <dvelastegui@yahoo.com>
2020-03-10 00:57:48 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 describe "say_hello" do 
   
   it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Kent Beck!")
+    expect($stdout).to receive(:puts).with("Gabriela!")
     say_hello("Kent Beck")
   end
     it 'defaults to Ruby Programmer when no name is passed in' do
